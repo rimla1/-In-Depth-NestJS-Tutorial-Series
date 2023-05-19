659d6402f1b82fe910c78e6df9f5f025010f3181
Almir Muminovic <projectalmir@gmail.com>
2023-05-19 11:22:24 +0200

    Making request on more specific route

--- src/users/controllers/users/users.controller.ts
+++ src/users/controllers/users/users.controller.ts
@@ -2,8 +2,15 @@ import { Controller, Get } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
+
     @Get()
     getUsers(){
-        return {name: "Test", email: "test@test.com"}
+        return [{name: "Test", email: "test@test.com"}, {name: "Test2", email: "test2@test.com"}, {name: "Test3", email: "test3@test.com"}]
     }
+
+    @Get('posts')
+    getUsersPosts(){
+        return [{name: "Test", email: "test@test.com", posts: [{id: 1, title: "Post 1"}, {id: 2, title: "Post 2"}]}, {name: "Test3", email: "test3@test.com", posts: [{id: 3, title: "Post 3"}, {id: 2, title: "Post 4"}]}]
+    }
+
 }
